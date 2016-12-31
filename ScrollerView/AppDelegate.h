93300d6086e3bfb179c2471ede34666b464703b7
PEEEATING <491701599@qq.com>
2016-12-31 16:42:52 +0800

    Initial Commit

---
+++ ScrollerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollerView
+//
+//  Created by EATING on 2016/12/31.
+//  Copyright © 2016年 EATING. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollerView
+//
+//  Created by EATING on 2016/12/31.
+//  Copyright © 2016年 EATING. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
